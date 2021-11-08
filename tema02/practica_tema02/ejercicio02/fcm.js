cbb09224e9d1b60913a794b534886d67b41c746b
dawjsierra <sierra.javier.daw2@gmail.com>
2021-11-08 03:32:11 +0100

    ejercicios 3 y 4

--- tema02/practica_tema02/ejercicio02/fcm.js
+++ tema02/practica_tema02/ejercicio02/fcm.js
@@ -2,21 +2,29 @@ function init(){
     var edad;
     var sexo;
     var aux;
+    //He creado un patrón que solo pueda ser H o M, porque no sabía si se podían comparar cadenas como en Java.
+    //En cualquier caso... Funciona.
     var patSexo = /[MH]/;
-
+    
+    //Mientras que edad sea una cadena... Lo va a pedir
     do{
         edad = parseInt(prompt("Introduzca su edad:"));
     }while(isNaN(edad)==true);
-
     
+
+    //Mientras la comprobación del patrón de false, lo pedirá
     do{
         sexo = prompt("Introduzca su sexo. Hombre (H) - Mujer (M)");
         aux = patSexo.test(sexo);
     }while( aux==false);
-
+    
+    //Almacenamos y calculamos la fcm
     var fcm = 220 - edad;
+    
 
+    //Calculamos porcentajes
     var sesenta = fcm*0.7;
+    //Fijamos los decimales a dos
     sesenta = sesenta.toFixed(2);
     var setenta = fcm*0.7;
     setenta = setenta.toFixed(2);
@@ -26,17 +34,18 @@ function init(){
     noventa = noventa.toFixed(2);
 
 
-
+    //En li, almacenamos todos los elementos <li> del documento
     var li = document.getElementsByTagName("li");
-    var parrafo = document.getElementsByTagName("p");
+    //Nos creamos una variable para indicar la FCM de cada usuario...
     var tufcm = document.createTextNode(fcm);
 
+    //Accedemos al array de li mediante [posicion] y con .innerHTML añadimos la frase
     li[0].innerHTML += sesenta +" - "+setenta;
     li[1].innerHTML += setenta +" - "+ochenta;
     li[2].innerHTML += ochenta +" - "+noventa;
     li[3].innerHTML += noventa +" - "+fcm;
     document.getElementById("fcm").appendChild(tufcm);
-    document.getElementById("body").appendChild(tabla)
+    document.getElementById("body").appendChild(tabla);
     
 
     
--- tema02/practica_tema02/ejercicio03/horario.js
+++ tema02/practica_tema02/ejercicio03/horario.js
@@ -1,19 +1,23 @@
 function init(){
 
-    var actividades = ["Crossfit","Zumba","Bodypump","Spinning","Pilates"];
+    //introducimos datos en el array
+    var actividades = ["Crossfit","Zumba","Bodypump","Spinning","Pilates","Yoga","Kickboxing"];
     var fisios = ["Marta","Marcos","Sergio","Laura","Paula"];
 
-    
-    var fila = document.createElement("tr");
-    var celda = document.createElement("td");
+    //seleccionamos los elementos con el id de las tablas a escoger
     var tablaAct = document.getElementById("actividades");
     var tablaFisios = document.getElementById("fisios");
-    tablaAct.setAttribute("border",1);
-    tablaAct.setAttribute("border",1);
 
+    //les damos bordes
+    tablaAct.setAttribute("border",1);
+    tablaFisios.setAttribute("border",1);
+
+    //seleccionamos las filas de la tabla de actividades (tablerow)
     var filasAct = tablaAct.getElementsByTagName("tr");
+    //seleccionamos las filas de la tabla de fisios
     var filasFis = tablaFisios.getElementsByTagName("tr");
 
+    //for anidado. Primero recorrerá las filas y pasará a la siguiente columna rellenandolo con el array (random)
     for(var i = 1; i<7;i++){
          for(j = 1; j<7;j++){
             var aleatorio = Math.round(Math.random()*4);
@@ -21,12 +25,12 @@ function init(){
          }
     }
 
-    for(var i = 1; i<11;i++){
+    //similar al anterior
+    for(var i = 1; i<10;i++){
         for(j = 1; j<6;j++){
             var aleatorio = Math.round(Math.random()*4);
             filasFis[i].innerHTML += "<td>"+fisios[aleatorio]+"</td>";
          }
     }
-
 
 }
